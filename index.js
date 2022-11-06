851e0b3f1c36a1f3c0c384576296c604ab252713
yousif <yousifr.ismail@gmail.com>
2022-11-06 22:37:35 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName="yousif";
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+function setBestCustomer(){
+    var bestCustomer="not bob"
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer="hi bob"
+}
+
+const leastFavoriteCustomer='helleo'
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer="hi"
+}
+
+console.log(upperCaseCustomerName())
